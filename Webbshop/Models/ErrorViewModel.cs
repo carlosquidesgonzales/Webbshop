2cc1a4ed8f06e4db1b182f2d8c65d4e5fefcfb6e
Carlos Gonzales <crls.gonzales@outlook.com>
2021-01-20 11:33:45 +0100

    Add project files.

---
+++ Webbshop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webbshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
